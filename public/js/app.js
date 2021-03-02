fc4d536a2e1ab679135728ae13596a0f9a7470e3
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-03-02 14:06:40 +0200

    dont marge

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-// 
+'use strict'
+
+// function burger() {
+
+//     var x = document.getElementById("nav");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+
+
+// //   $('.burger').on('click' , );
+
+//   $( '.burger' ).click(function() {
+//     $( '#nav' ).slideToggle();
+//   });
+// console.log("hello");
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 "use strict"
 
+
 // Import packages
 const express = require('express');
 const cors = require('cors');
